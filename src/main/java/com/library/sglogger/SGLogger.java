25e50ed2701afab810c561f76d8570b3cd1c93fc
smallgod <davies.mugume@gmail.com>
2016-12-17 20:33:13 +0300

    December 17th, 2016 20hr30

--- src/main/java/com/library/sglogger/SGLogger.java
+++ src/main/java/com/library/sglogger/SGLogger.java
@@ -5,7 +5,16 @@
  */
 package com.library.sglogger;
 
+import com.library.utilities.DateUtils;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.openide.util.MapFormat;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -13,58 +22,120 @@ import org.slf4j.Logger;
  */
 public final class SGLogger {
 
-    private final Logger LOGGER;
+    private Logger LOGGER;
+    
 
-    public SGLogger(final Logger LOGGER) {
-        System.out.println("this guy is calledddd!!!");
-        this.LOGGER = LOGGER;
+    public SGLogger() throws FileNotFoundException, Exception {
+        //this.configureLog4J(log4jFile);
+    }
+
+    public <T> void addLogger(final Class<T> loggerClass) {
+        this.LOGGER = LoggerFactory.getLogger(loggerClass);
     }
 
     public void debug(String debugLogText) {
 
+        String log = formatLog(debugLogText);
+
         if (this.LOGGER != null) {
-            this.LOGGER.debug(debugLogText);
+            this.LOGGER.debug(log);
         } else {
-            System.out.println("DEBUG: " + debugLogText);
+            System.out.println(log);
         }
     }
 
     public void info(String infoLogText) {
 
+        String log = formatLog(infoLogText);
+
         if (LOGGER != null) {
-            LOGGER.info(infoLogText);
+            LOGGER.info(log);
         } else {
-            System.out.println("INFO: " + infoLogText);
+            System.out.println(log);
         }
     }
 
     public void error(String errorLogText) {
 
+        String log = formatLog(errorLogText);
+
         if (LOGGER != null) {
-            LOGGER.error(errorLogText);
+            LOGGER.error(log);
         } else {
-            System.err.println("ERROR: " + errorLogText);
+            System.err.println(log);
         }
     }
 
     public void warn(String warnLogText) {
 
+        String log = formatLog(warnLogText);
+
         if (LOGGER != null) {
-            LOGGER.warn(warnLogText);
+            LOGGER.warn(log);
         } else {
-            System.out.println("WARNING: " + warnLogText);
+            System.out.println(log);
         }
     }
 
     public void fatal(String fatalLogText) {
 
+        String log = formatLog(fatalLogText);
+
         if (LOGGER != null) {
-            LOGGER.error(fatalLogText);
+            LOGGER.error(log);
             System.exit(-1);
         } else {
-            System.err.println("FATAL: " + fatalLogText);
+            System.err.println(log);
             System.exit(-1);
         }
+
     }
 
+    /**
+     * Format Log message
+     *
+     * @param logText
+     * @return
+     */
+    private String formatLog(String logText) {
+
+        String log = "[{dateTime}] [{logger}] - {logText}";
+
+        Map<String, Object> map = new HashMap<>();
+
+        map.put("dateTime", DateUtils.getDefaultDateTimeNow());
+        map.put("logger", this.LOGGER != null ? this.LOGGER.getClass().getSimpleName() : "LOGGER is NULL");
+        map.put("logText", logText);
+
+        //return (MapFormat.format(log, map));
+        return logText;
+
+    }
+
+    /**
+     * Configure the log4J2.xml file
+     *
+     * @param log4jFile
+     * @throws java.io.FileNotFoundException
+     * @throws java.lang.Exception
+     */
+    public static void configureLog4J(String log4jFile) throws FileNotFoundException, Exception {
+
+        System.out.println("configureLog4J Called, yay!");
+        
+        LoggerContext context = (LoggerContext) LogManager.getContext(false);
+        File file = new File(log4jFile);
+        if (file.exists()) {
+            context.setConfigLocation(file.toURI());
+        } else {
+            throw new FileNotFoundException("Failed to find file @: " + log4jFile);
+        }
+
+        //Properties props = new Properties();
+        //props.put("logsFolder", paramsToPass);
+        //DOMConfigurator.setParameter(elem, propSetter, props);
+        //DOMConfigurator.configure(log4jFile); //XML configurator
+        //MapLookup.setMainArguments(paramsToPass);
+        //PropertyConfigurator.configure(log4jPropsFileLoc);//Property file configurator
+    }
 }
